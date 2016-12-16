6cdcb3095015de7501ca1362f7e73260b735a8ec
Macbook pro Retina <2634300188@qq.com>
2016-12-16 17:45:03 +0800

    modified repo a bit

--- repo.rb
+++ repo.rb
@@ -1,1 +1,1 @@
-<html><body>You are being <a href="https://raw.githubusercontent.com/mojombo/grit/master/lib/grit/repo.rb">redirected</a>.</body></html>
+<html><body>You are being <a href="https://raw.githubusercontent.com/mojombo/grit/master/lib/grit/repo.rb">redirected</a>.</body></html># testing
